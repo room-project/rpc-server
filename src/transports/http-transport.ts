2a6731f2b426e3ee7ff0c0b2234031acd77185ba
Dmitry Dudin <dima.dudin@gmail.com>
2021-03-09 15:05:56 +0700

    initial

---
+++ src/transports/http-transport.ts
@@ -0,0 +1,82 @@
+import { createEvent } from 'effector'
+import express, { Response } from 'express'
+import cors, { CorsOptions } from 'cors'
+import http from 'http'
+import {
+  IRpcServerTransport,
+  IRpcRequest,
+  IRpcResponse,
+  RpcMessageId,
+} from '@room-project/rpc-core'
+
+export interface IRpcServerHttpTransportOptions {
+  port: number
+  cors?: CorsOptions
+}
+
+export interface IRpcServerHttpTransportFactory {
+  (options: IRpcServerHttpTransportOptions): IRpcServerTransport
+  of: IRpcServerHttpTransportFactory
+}
+
+const parseRequest = (data: string): IRpcRequest => {
+  try {
+    const payload = JSON.parse(data)
+    return payload
+  } catch (error) {
+    throw new Error(`Invalid request format`)
+  }
+}
+
+export const RpcServerHttpTransportFactory: IRpcServerHttpTransportFactory = (options) => {
+  const queue = new Map<RpcMessageId, Response>()
+
+  const app = express()
+    .use(
+      cors({
+        origin: '*',
+        ...options.cors,
+      })
+    )
+    .post('/', async function handler(req, res) {
+      try {
+        const request = parseRequest(req.body)
+        queue.set(request.id, res)
+        receive(request)
+      } catch (error) {
+        res.status(400).end()
+      }
+    })
+
+  const server = http.createServer(app)
+
+  const open = async () => {
+    server.listen(options.port)
+  }
+
+  const close = async () => {
+    server.close()
+  }
+
+  const receive = createEvent<IRpcRequest>('receive')
+  const send = createEvent<IRpcResponse>('send')
+
+  send.watch((response) => {
+    const res = queue.get(response.id)
+    if (res) {
+      res.status(200).json(response)
+      queue.delete(response.id)
+    }
+  })
+
+  const transport = {
+    receive,
+    send,
+    open,
+    close,
+  }
+
+  return transport
+}
+
+RpcServerHttpTransportFactory.of = RpcServerHttpTransportFactory
